0066200d415d209beb758831e7741c5d6fea5a59
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-06-26 10:15:54 +0200

    Add files via upload

---
+++ box_office_DB.sql
@@ -0,0 +1,25 @@
+CREATE TABLE box_office (
+	id INT PRIMARY KEY,
+	movie VARCHAR (120),
+	city VARCHAR (120),
+	country VARCHAR (120), 
+	gross DECIMAL (10,2)
+
+	
+);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (1, 'Beau Is Afraid', 'Amsterdam', 'Netherlands', 483754.22);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (2, 'The French Dispatch', 'Valencia', 'Spain', 385741.59);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (3, 'Beau Is Afraid', 'Valencia', 'Spain', 40874.59);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (4, 'The French Dispatch', 'Madrid', 'Spain', 845125.98);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (5, 'Beau Is Afraid', 'Rotterdam', 'Netherlands', 352147.77);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (6, 'The French Dispatch', 'Den Haag', 'Netherlands', 100524.14);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (7, 'Beau Is Afraid', 'Madrid', 'Spain', 147874.56);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (8, 'The French Dispatch', 'Barcelona', 'Spain', 354789.52);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (9, 'Beau Is Afraid', 'Den Haag', 'Netherlands', 208471.52);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (10, 'The French Dispatch', 'Amsterdam', 'Netherlands', 408974.56);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (11, 'Beau Is Afraid', 'Barcelona', 'Spain', 205487.23);
+INSERT INTO box_office (id, movie, city, country, gross) VALUES (12, 'The French Dispatch', 'Rotterdam', 'Netherlands', 236974.54);
+
+
+
+
